fc366d4f549252ae269a2e070bb409e412a19179
Daniil_Khobotov <dinich44@gmail.com>
2022-11-06 13:19:41 +0400

    add event listenets

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
+import { Component } from 'react';
+
 import './App.css';
 
+class WhoAmI extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			years: 27,
+			text: '+++',
+			position: '',
+		}
+	}
+
+	nextYear = () => {
+		this.setState(state => ({
+			years: state.years + 1
+		}))
+	}
+
+	commitInputChanges = (e, color) => {
+		console.log(color);
+		
+		this.setState({
+			position: e.target.value
+		})
+	}
+
+	render() {
+		const {name, surname, link} = this.props;
+		const {position, years, text} = this.state;
+
+		return (
+			<div>	
+				<h1>My name is {name}, surname - {surname},
+					age - {years},
+					position - {position}</h1>
+				<button onClick={this.nextYear}>{text}</button>
+				<br/>
+				<a href={link}>My profile</a>
+				<form>
+					<span>Введите должность</span>
+					<input type="text" onChange={(e) => this.commitInputChanges(e, 'some color')}/>
+				</form>
+
+			</div>
+		)
+	}
+}
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<WhoAmI name='John' surname="Smith" link="fb.com"/>
+			<WhoAmI name='Alex' surname="Shepard" link="vk.com"/>
+		</div>
+	);
 }
 
 export default App;
