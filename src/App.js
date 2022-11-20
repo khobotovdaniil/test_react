3df72c098e1ab18e10a423a3f42cb8eaaf269d33
Daniil_Khobotov <dinich44@gmail.com>
2022-11-20 12:13:49 +0400

    add props.render

--- src/App.js
+++ src/App.js
@@ -98,9 +98,56 @@ const DynamicGreeting = (props) => {
 	)
 }
 
+const HelloGreeting = () => {
+	return (
+		<div style={{'width': '600px', 'margin': '0 auto'}}>
+			<DynamicGreeting color={'primary'}>
+				<h2>Hello World!</h2>
+			</DynamicGreeting>
+		</div>
+	)
+}
+
+const Message = (props) => {
+	return (
+		<h2>The counter is {props.counter}</h2>
+	)
+}
+
+class Counter extends Component {
+	state = {
+		counter: 0
+	}
+
+	changeCounter = () => {
+		this.setState(({counter}) => ({
+			counter: counter + 1
+		}))
+	}
+
+	render() {
+		return (
+			<>
+				<button
+					className={'btn btn-primary'}
+					onClick={this.changeCounter}>
+					Click me
+				</button>
+				{this.props.render(this.state.counter)}
+			</>
+		)
+	}
+}
+
 function App() {
 	return (
 		<Wrapper>
+
+			<Counter render={counter => (
+				<Message counter={counter}/>
+			)}/>
+
+			<HelloGreeting/>
 			<BootstrapTest
 				left = {
 					<DynamicGreeting color={'primary'}>
