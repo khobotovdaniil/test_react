dfbdf35b4d868b975b892a4851f81c3c099a293c
Daniil_Khobotov <dinich44@gmail.com>
2022-11-26 11:20:24 +0400

    add  custom hook

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import ReactDOM from 'react-dom';
 import styled from 'styled-components';
 import BootstrapTest from './BootstrapTest';
@@ -140,6 +140,57 @@ class Counter extends Component {
 	}
 }
 
+//custom hook
+function useInputWithValidate(initialValue) {
+	const [value, setValue] = useState(initialValue);
+
+	const onChange = event => {
+		setValue(event.target.value);
+	}
+
+	const validateInput = () => {
+		return value.search(/\d/) >= 0
+	}
+
+	return {value, onChange, validateInput} 
+}
+
+const FormForNewHooks = () => {
+	const input = useInputWithValidate('');
+	const textArea = useInputWithValidate('');
+
+	const color = input.validateInput() ? 'text-danger' : null;
+
+	return (
+		<Container>
+			<form className="w-50 border mt-5 p-3 m-auto">
+				<div className="mb-3">
+					<input value={`${input.value} / ${textArea.value}`} type="text" className="form-control" readOnly />
+					<label htmlFor="exampleFormControlInput1" className="form-label mt-3">Email address</label>
+					<input 
+						onChange={input.onChange}
+						type="email"
+						value={input.value}
+						className={`form-control ${color}`}
+						id="exampleFormControlInput1" 
+						placeholder="name@example.com"/>
+				</div>
+				<div className="mb-3">
+					<label htmlFor="exampleFormControlTextarea1" className="form-label">Example textarea</label>
+					<textarea
+						onChange={textArea.onChange}
+						value={textArea.value}
+						className="form-control"
+						id="exampleFormControlTextarea1"
+						rows="3">
+					</textarea>
+				</div>
+			</form>
+		</Container>
+	)
+}
+
+// Refs
 class RefForm extends Component {
 	// myRef = React.createRef();
 
@@ -176,6 +227,7 @@ class RefForm extends Component {
     }
 }
 
+//portals
 class NewForm extends Component {
 	state = {
 		advOpen: false
@@ -238,6 +290,7 @@ const Msg = () => {
 function App() {
 	return (
 		<>
+			<FormForNewHooks/>
 			<RefForm/>
 			<Wrapper>
 				<Counter render={counter => (
